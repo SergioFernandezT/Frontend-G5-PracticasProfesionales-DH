5e07948698de026ec7a8b5746f4286216fdc4e36
Maria-Vedia <vediamarii@gmail.com>
2024-07-20 09:18:19 -0300

    agregue el listado de profesiones y faltar agregar las tarjetas de los aspirantes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="text-centre mb-8">
-        <h1 className='text-2x1 font-bold'>Aspirantes</h1>
+     <h1 className='text-2x1 font-bold'>Aspirantes</h1>
+    </div>
+    <div className='flex mt-5 gap-4 flex-wrap justify-centre'>
+      <div className='flex justify-around mb-8'>
+        <CardAspirante/> 
       </div>
-      <div className='flex mt-5 gap-4 flex-wrap justify-centre'>
-        <div className='flex justify-around mb-8'>
-          <CardAspirante/> 
-        </div>
-      </div>
+    </div>
 
       <div className='text-center mb-4'>
         <h2 className=' text-2xl font-bold'>Profesiones</h2>
