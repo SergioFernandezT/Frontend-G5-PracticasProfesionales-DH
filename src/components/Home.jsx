7ac5b4d7f848ae8676084a5899b8d1eda88e298a
Maria-Vedia <vediamarii@gmail.com>
2024-07-20 09:03:54 -0300

    agregue el listado de profesiones y falta terminar con las tarjetas de los aspirantes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import CardAspirante from './aspirantes/CardAspirante'
 
+import ListadoProfesiones from './profesiones/ListadoProfesiones'
 export default function Home() {
   return (
-    <div>Home</div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="text-centre mb-8">
+        <h1 className='text-2x1 font-bold'>Aspirantes</h1>
+      </div>
+      <div className='flex mt-5 gap-4 flex-wrap justify-centre'>
+        <div className='flex justify-around mb-8'>
+          <CardAspirante/> 
+        </div>
+      </div>
+
+      <div className='text-center mb-4'>
+        <h2 className=' text-2xl font-bold'>Profesiones</h2>
+      </div>
+         <ListadoProfesiones/>
+    </div>
   )
 }
