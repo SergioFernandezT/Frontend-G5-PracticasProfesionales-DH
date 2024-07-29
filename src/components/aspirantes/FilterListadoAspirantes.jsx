c8e8528eabecb93428e25626940cd75f87945dd9
Maria-Vedia <vediamarii@gmail.com>
2024-07-29 15:37:00 -0300

    agregue paginado al filter del listado de aspirantes

--- src/components/aspirantes/FilterListadoAspirantes.jsx
+++ src/components/aspirantes/FilterListadoAspirantes.jsx
@@ -63,7 +63,57 @@ export default function FilterListadoAspirantes() {
             ))}
           </div>
         </section>
-      </div>
-    );
+
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+          <div className="flex flex-1 justify-between sm:hidden">
+            <button
+              onClick={handlePreviousPage}
+              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+              disabled={currentPage === 1}>
+              Anterior
+           </button>
+
+           <button
+             onClick={handleNextPage}
+             className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+             disabled={currentPage === totalPages}>    
+             Siguiente
+           </button>
+
+         </div>
+         <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+    
+            <div aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+              <button
+                onClick={handlePreviousPage}
+                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                disabled={currentPage === 1}
+              >
+                <span className="sr-only">Anterior</span>
+                <ChevronLeftIcon aria-hidden="true" className="h-5 w-5" />
+              </button>
+
+              {[...Array(totalPages)].map((_, index) => (
+                <button
+                key={index + 1}
+                onClick={() => setCurrentPage(index + 1)}
+                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${index + 1 === currentPage ? 'bg-indigo-600 text-white' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'} focus:z-20 focus:outline-offset-0`}>  
+                {index + 1}
+                </button>
+              ))}
+
+              <button
+                onClick={handleNextPage}
+                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                disabled={currentPage === totalPages}>   
+                          
+                <span className="sr-only">Siguiente</span>
+                <ChevronRightIcon aria-hidden="true" className="h-5 w-5" />
+              </button>
+              </div>
+            </div>
+            </div>
+          </div>
+        );
   }
 
