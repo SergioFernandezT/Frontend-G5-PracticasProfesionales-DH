586586d48fcb69a80f886f92091cbde79d86d1ef
MerSb <emilcemercedes2@gmail.com>
2024-07-29 15:14:21 -0300

    boton editar

--- src/components/aspirantes/DetailAspirante.jsx
+++ src/components/aspirantes/DetailAspirante.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { getAspiranteById, updateAspirante } from "../servicios/aspirantesService";
+import {
+  getAspiranteById,
+  updateAspirante,
+} from "../servicios/aspirantesService";
+import { getAllProfesiones } from "../servicios/profesionesService";
 
 function AspiranteDetail() {
   const { id } = useParams();
@@ -8,6 +12,8 @@ function AspiranteDetail() {
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState({});
   const navigate = useNavigate();
+  const [profesiones, setProfesiones] = useState([]);
+  const [imageFile, setImageFile] = useState(null); // Nuevo estado para la imagen
 
   useEffect(() => {
     // Fetch aspirante data using the service function
@@ -18,6 +24,19 @@ function AspiranteDetail() {
       })
       .catch((error) => console.error("Error:", error));
   }, [id]);
+
+  useEffect(() => {
+    const fetchProfesiones = async () => {
+      try {
+        const data = await getAllProfesiones();
+        setProfesiones(data);
+      } catch (error) {
+        console.error("Error al cargar las profesiones", error);
+      }
+    };
+
+    fetchProfesiones();
+  }, []);
 
   const calculateAge = (birthdate) => {
     const birthDate = new Date(birthdate);
@@ -42,19 +61,34 @@ function AspiranteDetail() {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Call the updateAspirante function from your service
-    updateAspirante(id, formData)
+    const data = new FormData();
+    for (const key in formData) {
+      data.append(key, formData[key]);
+    }
+    delete data.profesiones_de_aspirante
+    if (imageFile) {
+      data.append("imagen", imageFile[0]);
+    }
+
+    updateAspirante(id, data)
       .then((updatedData) => {
         setAspirante(updatedData);
         setIsEditing(false); // Exit edit mode after successful update
       })
       .catch((error) => console.error("Error updating aspirante:", error));
   };
+
   if (!aspirante) {
     return <div>Loading...</div>;
   }
+
   if (localStorage.id == id) {
     return (
       <div className="p-4 h-auto md:min-h-dvh w-full flex flex-col gap-4 md:items-start">
@@ -111,6 +145,15 @@ function AspiranteDetail() {
             </>
           ) : (
             <form onSubmit={handleFormSubmit} className="space-y-4">
+              <div>
+                <label className="block font-bold">Imagen:</label>
+                <input
+                  type="file"
+                  name="imagen"
+                  onChange={handleImageChange}
+                  className="w-full p-2 border rounded"
+                />
+              </div>
               <div>
                 <label className="block font-bold">Nombre:</label>
                 <input
@@ -185,6 +228,25 @@ function AspiranteDetail() {
                   <option value="Otro">Otro</option>
                 </select>
               </div>
+
+              <div>
+                <select
+                  id="profesion_id"
+                  name="profesion_id"
+                  value={formData.profesion_id}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option value="">Seleccione una Profesion</option>
+                  {profesiones.map((profesion) => {
+                    return (
+                      <option key={profesion.id} value={profesion.id}>
+                        {profesion.profesion}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
               <button
                 type="submit"
                 className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
@@ -205,31 +267,32 @@ function AspiranteDetail() {
   }
   return (
     <div className="p-4 h-auto md:min-h-dvh w-full flex flex-col gap-4 md:items-start">
-      <div className='w-full px-3 pb-3 border-b-2 flex flex-col sm:flex-row items-center justify-between'>
+      <div className="w-full px-3 pb-3 border-b-2 flex flex-col sm:flex-row items-center justify-between">
         <img
           src={aspirante.imagen || "default_cover_img_url"}
           alt={`${aspirante.nombre} ${aspirante.apellido}'s cover`}
           className="bg-gray-200 w-52 h-52 object-cover rounded-lg"
         />
-        <div className='w-[75%] p-4'>
+        <div className="w-[75%] p-4">
           <div className="flex flex-col items-center md:items-start py-2 mb-4">
             <h2 className="text-2xl font-semibold">{`${aspirante.nombre} ${aspirante.apellido}`}</h2>
             <p className="text-gray-600 text-xl">{aspirante.profesion}</p>
           </div>
-          <div className='text-start text-xl mb-4'>
+          <div className="text-start text-xl mb-4">
             <p>{aspirante.descripcion}</p>
           </div>
         </div>
       </div>
       <div className="flex flex-col justify-around text-start text-xl h-80 w-full p-5 border shadow-sm rounded-lg">
         <p>
-          <strong className='font-bold' >Email: </strong>{aspirante.email}
+          <strong className="font-bold">Email: </strong>
+          {aspirante.email}
         </p>
         <p>
-          <strong className='font-bold' >Teléfono: </strong> {aspirante.telefono}
+          <strong className="font-bold">Teléfono: </strong> {aspirante.telefono}
         </p>
         <p>
-          <strong className='font-bold' >LinkedIn: </strong>
+          <strong className="font-bold">LinkedIn: </strong>
           <a
             href={aspirante.perfil_linkedin}
             className="text-blue-500 hover:underline"
@@ -238,16 +301,21 @@ function AspiranteDetail() {
           </a>
         </p>
         <p>
-          <strong className='font-bold' >Fecha de Nacimiento: </strong>{aspirante.fecha_nacimiento}
+          <strong className="font-bold">Fecha de Nacimiento: </strong>
+          {aspirante.fecha_nacimiento}
         </p>
         <p>
-          <strong className='font-bold' >Edad: </strong>{calculateAge(aspirante.fecha_nacimiento)}
+          <strong className="font-bold">Edad: </strong>
+          {calculateAge(aspirante.fecha_nacimiento)}
         </p>
         <p>
-          <strong className='font-bold' >Sexo: </strong>{aspirante.sexo}</p>
+          <strong className="font-bold">Sexo: </strong>
+          {aspirante.sexo}
+        </p>
       </div>
     </div>
   );
 }
 
 export default AspiranteDetail;
+
